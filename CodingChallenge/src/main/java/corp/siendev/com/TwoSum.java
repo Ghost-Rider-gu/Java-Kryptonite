17027494fd481af44c5f051405129d2b5ce2a8d2
Iurii Golubnichenko <ghostrider.gu@gmail.com>
2022-11-15 18:45:35 +0200

    added CodingChallenge part with the first example

---
+++ CodingChallenge/src/main/java/corp/siendev/com/TwoSum.java
@@ -0,0 +1,35 @@
+/**
+ * @author Iurii Golubnichenko
+ *
+ * There is an array of integers and an integer target.
+ * You need to write a function / method that will return indices of the two numbers such that they add up to target.
+ *
+ * Example
+ * ==================================================================
+ * Input: nums = [3, 7, 15, 5, 4], target = 9
+ * Output: [5, 4] -> because 5 + 4 == 9
+ * ==================================================================
+ *
+ * For the solution below:
+ * Time complexity will be (O)n and Space complexity (O)n -> because we use additional structure to save data.
+ */
+package corp.siendev.com;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class TwoSum {
+    public static int[] sumOfTwoNumbers(int[] nums, int target) {
+        Set<Integer> potentialCandidates = new HashSet<>();
+
+        for (int firstNumber: nums) {
+            int secondNumber = target - firstNumber;
+            if (potentialCandidates.contains(secondNumber)) {
+                return new int[] { secondNumber, firstNumber };
+            } else {
+                potentialCandidates.add(firstNumber);
+            }
+        }
+        return new int[0];
+    }
+}
---
+++ CodingChallenge/src/test/java/corp/siendev/com/TwoSumTests.java
@@ -0,0 +1,23 @@
+/**
+ * @author Iurii Golubnichenko
+ *
+ * Test cases for TwoSum challenge.
+ */
+package corp.siendev.com;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TwoSumTests {
+    private static final int[] numsExample = new int[] { 3, 7, 15, 5, 4 };
+    private static final int[] expectedResult = new int[] { 5, 4 };
+    private static final int targetSum = 9;
+
+    @Test
+    public void sumOfTwoNumbersTest() {
+        int[] result = TwoSum.sumOfTwoNumbers(numsExample, targetSum);
+
+        Assertions.assertNotNull(result);
+        Assertions.assertArrayEquals(expectedResult, result);
+    }
+}
