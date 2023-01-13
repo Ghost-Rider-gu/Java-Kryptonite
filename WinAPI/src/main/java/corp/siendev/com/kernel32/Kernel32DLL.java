1e9faf82505c966adc61f381449eb761cfae0e50
Iurii Golubnichenko <ghostrider.gu@gmail.com>
2023-01-13 23:14:56 +0200

    Kernel32 DLL start working on

---
+++ WinAPI/src/main/java/corp/siendev/com/kernel32/Kernel32DLL.java
@@ -0,0 +1,19 @@
+package corp.siendev.com.kernel32;
+
+import com.sun.jna.Platform;
+import com.sun.jna.platform.win32.Kernel32;
+import com.sun.jna.platform.win32.WinBase;
+import com.sun.jna.ptr.IntByReference;
+
+public class Kernel32DLL {
+    public static void main(String[] args) {
+        IntByReference intByReference = new IntByReference(0);
+        char[] buffer = new char[WinBase.MAX_COMPUTERNAME_LENGTH + 1];
+        intByReference.setValue(buffer.length);
+        System.out.println(Kernel32.INSTANCE.GetComputerName(buffer, intByReference));
+    }
+
+    public static boolean isWinPlatform() {
+        return Platform.isWindows();
+    }
+}
