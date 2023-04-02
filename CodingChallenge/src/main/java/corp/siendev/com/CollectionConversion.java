7acd1777756be51ee6bf45ac9b2c3de56ec5b6b5
Iurii Golubnichenko <ghostrider.gu@gmail.com>
2023-04-02 21:12:31 +0300

    Added CollectionConversion class (some common questions)

---
+++ CodingChallenge/src/main/java/corp/siendev/com/CollectionConversion.java
@@ -0,0 +1,51 @@
+/**
+ * @author Iurii Golubnichenko
+ *
+ * Some common questions on interview regarding Collections
+ *
+ * For the same results you can use Apache Commons or Google Guava libraries.
+ * In here we use only Java Core.
+ *
+ */
+package corp.siendev.com;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public final class CollectionConversion {
+    public static List<Integer> convertArrayToListFirst(int[] arrayOfIntegers) {
+        final List<Integer> listOfIntegers = new ArrayList<>();
+        for (int element: arrayOfIntegers) {
+            listOfIntegers.add(element);
+        }
+
+        return listOfIntegers;
+    }
+
+    public static List<Integer> convertArrayToListSecond(int[] arrayOfIntegers) {
+        return IntStream.of(arrayOfIntegers)
+                .boxed()
+                .collect(Collectors.toList());
+        /**
+         * or you can simply use Arrays
+         * return Arrays.stream(arrayOfIntegers).boxed().collect(Collectors.toList());
+         */
+    }
+
+    public static int[] convertListToArrayFirst(List<Integer> listOfIntegers) {
+        final int[] arrayOfIntegers = new int[listOfIntegers.size()];
+        for (int i = 0; i < listOfIntegers.size(); i++) {
+            arrayOfIntegers[i] = listOfIntegers.get(i);
+        }
+
+        return arrayOfIntegers;
+    }
+
+    public static int[] convertListToArraySecond(List<Integer> listOfIntegers) {
+        return listOfIntegers.stream()
+                .mapToInt(Integer::intValue)
+                .toArray();
+    }
+}
