4b1ba54724cf51a9cb7790dae58ecf0627b1d1d0
Iurii Golubnichenko <Iurii_Golubnichenko@epam.com>
2023-04-24 14:23:53 +0300

    Updated TwoSum algo + tests

--- CodingChallenge/src/main/java/corp/siendev/com/TwoSum.java
+++ CodingChallenge/src/main/java/corp/siendev/com/TwoSum.java
@@ -1,8 +1,8 @@
 /**
  * @author Iurii Golubnichenko
  *
- * There is an array of integers and an integer target.
- * You need to write a function / method that will return indices of the two numbers such that they add up to target.
+ * There is an array of integers and an integer as a target.
+ * You need to write a method that will return indices of the two numbers such that they add up to target.
  *
  * Example
  * ==================================================================
@@ -10,22 +10,76 @@
  * Output: [5, 4] -> because 5 + 4 == 9
  * ==================================================================
  *
- * For the solution below:
- * Time complexity will be (O)n and Space complexity (O)n -> because we use additional structure to save data.
  */
 package corp.siendev.com;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 public final class TwoSum {
+    /**
+     * Solution with loops.
+     * Time complexity will be O(n^2) and Space complexity O(1).
+     *
+     * @param nums array of integers
+     * @param target the target sum of two numbers from given array
+     * @return int[x, y]
+     */
+    public static int[] sumOfTwoNumbersWithLoops(int[] nums, int target) {
+        for (int i = 0; i <= nums.length - 1; i++) {
+            int firstNumber = nums[i];
+            for (int j = i + 1; j <= nums.length - 1; j++) {
+                int secondNumber = nums[j];
+                if (firstNumber + secondNumber == target) {
+                    return new int[] { firstNumber, secondNumber };
+                }
+            }
+        }
+        return new int[0];
+    }
+
+    /**
+     * Solution that relly on sorting + left and right pointers of the array.
+     * Time complexity will be O(n log(n)) and Space complexity O(1).
+     *
+     * @param nums array of integers
+     * @param target the target sum of two numbers from given array
+     * @return int[x, y]
+     */
+    public static int[] sumOfTwoNumbersWithPointers(int[] nums, int target) {
+        int leftPointer = 0;
+        int rightPointer = nums.length - 1;
+
+        Arrays.sort(nums);
+
+        while (leftPointer < rightPointer) {
+            int sum = nums[leftPointer] + nums[rightPointer];
+            if (sum == target) {
+                return new int[] { nums[leftPointer], nums[rightPointer] };
+            }
+            if (sum < target) {
+                leftPointer++;
+            } else {
+                rightPointer--;
+            }
+        }
+        return new int[0];
+    }
+
+    /**
+     * Time complexity will be O(n) and Space complexity O(n) -> because we use additional structure to save data.
+     *
+     * @param nums array of integers
+     * @param target the target sum of two numbers from given array
+     * @return int[x, y]
+     */
     public static int[] sumOfTwoNumbers(int[] nums, int target) {
         Set<Integer> potentialCandidates = new HashSet<>();
-
         for (int firstNumber: nums) {
             int secondNumber = target - firstNumber;
             if (potentialCandidates.contains(secondNumber)) {
-                return new int[] { secondNumber, firstNumber };
+                return new int[] { firstNumber, secondNumber };
             } else {
                 potentialCandidates.add(firstNumber);
             }
--- CodingChallenge/src/test/java/corp/siendev/com/TwoSumTest.java
+++ CodingChallenge/src/test/java/corp/siendev/com/TwoSumTest.java
@@ -5,19 +5,29 @@
  */
 package corp.siendev.com;
 
-import org.junit.jupiter.api.Assertions;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TwoSumTest {
-    private static final int[] numsExample = new int[] { 3, 7, 15, 5, 4 };
+    private static final int[] nums = new int[] { 3, 7, 15, 5, 4 };
     private static final int[] expectedResult = new int[] { 5, 4 };
     private static final int targetSum = 9;
 
     @Test
     public void sumOfTwoNumbersTest() {
-        int[] result = TwoSum.sumOfTwoNumbers(numsExample, targetSum);
+        int[] result = TwoSum.sumOfTwoNumbers(nums, targetSum);
+        Assertions.assertThat(expectedResult).contains(result);
+    }
 
-        Assertions.assertNotNull(result);
-        Assertions.assertArrayEquals(expectedResult, result);
+    @Test
+    public void sumOfTwoNumbersWithLoopsTest() {
+        int[] result = TwoSum.sumOfTwoNumbersWithLoops(nums, targetSum);
+        Assertions.assertThat(expectedResult).contains(result);
+    }
+
+    @Test
+    public void sumOfTwoNumbersWithPointersTest() {
+        int[] result = TwoSum.sumOfTwoNumbersWithPointers(nums, targetSum);
+        Assertions.assertThat(expectedResult).contains(result);
     }
 }
