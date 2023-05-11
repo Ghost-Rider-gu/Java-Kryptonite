0c84b9e0efbc048e6547513a80f1864bbef08e89
Iurii Golubnichenko <Iurii_Golubnichenko@epam.com>
2023-05-11 12:35:04 +0300

    Added Palindrome checking algo

---
+++ CodingChallenge/src/main/java/corp/siendev/com/PalindromeChecking.java
@@ -0,0 +1,62 @@
+/**
+ * @author Iurii Golubnichenko
+ * <p>
+ * Given a string and we need to check if that string is palindrome (reads the same in reverse order)
+ * As a result we must return True or False.
+ * </p>
+ * Example
+ * ======================================================================================================
+ * Input: givenString = "kayak";
+ * Output: true
+ * Input: givenString = "saippuakivikauppias";
+ * Output: true
+ * Input: givenString = "hello";
+ * Output: false
+ * ======================================================================================================
+ *
+ */
+package corp.siendev.com;
+
+public class PalindromeChecking {
+    /**
+     * Just using StringBuilder to solve the problem - all you need is Java :)
+     * @param givenString currentString
+     * @return true or false
+     */
+    public static boolean isPalindrome(String givenString) {
+        StringBuilder result = new StringBuilder(givenString);
+        result.reverse();
+        return givenString.contentEquals(result);
+    }
+
+    /**
+     * Time complexity O(n) and Space complexity O(1)
+     * @param givenString current string
+     * @return true or false
+     */
+    public static boolean isPalindromeWithPointers(String givenString) {
+        int leftIndex = 0;
+        int rightIndex = givenString.length() - 1;
+        while (leftIndex < rightIndex) {
+            if (givenString.charAt(leftIndex) != givenString.charAt(rightIndex)) {
+                return false;
+            }
+            leftIndex++;
+            rightIndex--;
+        }
+        return true;
+    }
+
+    /**
+     * Time complexity O(n) and Space complexity O(n)
+     * @param givenString current string
+     * @return true or false
+     */
+    public static boolean isPalindromeWithForLoop(String givenString) {
+        StringBuilder reversedString = new StringBuilder();
+        for (int i = givenString.length() - 1; i >= 0; i--) {
+            reversedString.append(givenString.charAt(i));
+        }
+        return givenString.contentEquals(reversedString);
+    }
+}
---
+++ CodingChallenge/src/test/java/corp/siendev/com/PalindromeCheckingTest.java
@@ -0,0 +1,40 @@
+/**
+ * @author Iurii Golubnichenko
+ *
+ * Test cases for Palindrome checking challenge.
+ */
+package corp.siendev.com;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class PalindromeCheckingTest {
+    private static final String stringExampleOne = "saippuakivikauppias";
+    private static final String stringExampleTwo = "kayak";
+    private static final String stringExampleThree = "deed";
+    private static final String stringExampleFour = "Hello World";
+
+    @Test
+    public void isPalindromeTest() {
+        boolean result = PalindromeChecking.isPalindrome(stringExampleOne);
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void isPalindromeWithPointersTest() {
+        boolean result = PalindromeChecking.isPalindromeWithPointers(stringExampleTwo);
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void isPalindromeWithForLoopTest() {
+        boolean result = PalindromeChecking.isPalindromeWithForLoop(stringExampleThree);
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void isNotPalindromeTest() {
+        boolean result = PalindromeChecking.isPalindrome(stringExampleFour);
+        Assertions.assertFalse(result);
+    }
+}
